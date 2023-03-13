8ae94f7a07ccbb3353f9b366e790a883d8381373
mbenson025 <mbenson025@gmail.com>
2023-03-12 21:58:23 -0500

    fix array issues combine both examples in same page

--- resources/views/examples.blade.php
+++ resources/views/examples.blade.php
@@ -6,8 +6,7 @@
     </div>
     <h2 class="text-lg font-semibold">Standard Contact Form</h2>
 
-    {{-- <livewire:contact-form /> --}}
-    <livewire:search-dropdown />
+    <livewire:contact-form />
 
     <hr>
 
